b0438bafc5e8a8e68df2d4cb24de43c1d097e709
Robert <nowak021@gmail.com>
2025-07-29 20:04:28 +0200

    add tray icon

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
+use tray_item::{IconSource, TrayItem};
+
 mod mouse_handler;
 
 fn main() {
     mouse_handler::move_silently();
+
+    let mut tray = TrayItem::new("Digital Vigilance", IconSource::Resource("")).unwrap();
+    tray.add_label("Set refresh interval").unwrap();
+
+    tray.add_menu_item("3 min", || {
+        println!("todo");
+    })
+    .unwrap();
+
+    let inner = tray.inner_mut();
+    inner.add_quit_item("Quit");
+    inner.display();
 }
