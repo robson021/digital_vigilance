157e41d66a81dcc6541704469fff3a1b042d38b3
Robert <nowak021@gmail.com>
2025-08-01 20:18:19 +0200

    add async task

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,27 @@
+use std::thread;
+use std::time::Duration;
 use tray_item::{IconSource, TrayItem};
 
 mod mouse_handler;
 
 fn main() {
-    mouse_handler::move_silently();
+    let sleep_time = 3 * 60;
+    let label = &format!("Refresh time: {sleep_time}s");
 
     let mut tray = TrayItem::new("Digital Vigilance", IconSource::Resource("")).unwrap();
-    tray.add_label("Set refresh interval").unwrap();
-
-    tray.add_menu_item("3 min", || {
-        println!("todo");
-    })
-    .unwrap();
+    tray.add_label(label).unwrap();
 
     let inner = tray.inner_mut();
     inner.add_quit_item("Quit");
+
+    thread::spawn(move || {
+        println!("Will move mouse every {sleep_time} seconds.");
+        let duration = Duration::from_secs(sleep_time);
+        loop {
+            thread::sleep(duration);
+            mouse_handler::move_silently()
+        }
+    });
+
     inner.display();
 }
