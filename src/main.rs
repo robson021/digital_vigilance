89571bac0642933b208b0f2b35647e700adf6c1c
Robert <nowak021@gmail.com>
2025-08-03 12:21:36 +0200

    add toast notification

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
+use notify_rust::Notification;
 use std::time::Duration;
 use tray_item::{IconSource, TrayItem};
 
 mod mouse_handler;
 
+const TITLE: &str = "Digital Vigilance";
+const REFRESH_TIME_MINUTES: u64 = 3;
+
 #[tokio::main(flavor = "multi_thread", worker_threads = 1)]
 async fn main() {
-    let minutes = 3;
-    let label = &format!("Refresh time: {minutes} minutes");
-
-    let mut tray = TrayItem::new("Digital Vigilance", IconSource::Resource("")).unwrap();
+    let mut tray = TrayItem::new(TITLE, IconSource::Resource("")).unwrap();
+    let label = &format!("Refresh time - every {REFRESH_TIME_MINUTES} minutes");
     tray.add_label(label).unwrap();
 
     tokio::spawn(async move {
         if cfg!(debug_assertions) {
-            println!("Will move mouse every {minutes} minutes.");
+            println!("Async task spawned")
         }
-        let duration = Duration::from_secs(minutes * 60);
+        let duration = Duration::from_secs(REFRESH_TIME_MINUTES * 60);
         loop {
             tokio::time::sleep(duration).await;
             mouse_handler::move_silently()
         }
     });
 
+    show_notification(label);
+
     let inner = tray.inner_mut();
     inner.add_quit_item("Quit");
     inner.display();
+}
+
+fn show_notification(message: &str) {
+    Notification::new()
+        .summary(TITLE)
+        .body(message)
+        .show()
+        .unwrap();
 }
