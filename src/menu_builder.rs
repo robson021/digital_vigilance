8bf5b83350bdcd7d52bd236bfb03ce14d13a51b7
Robert <nowak021@gmail.com>
2025-08-07 21:39:14 +0200

    polish

--- src/menu_builder.rs
+++ src/menu_builder.rs
@@ -10,10 +10,10 @@ pub fn build_menu(config: &SharedConfig) {
     let mut tray = TrayItem::new(APP_NAME, IconSource::Resource("")).unwrap();
     tray.add_label("Refresh time:").unwrap();
 
-    for min in [1, 3, 5] {
-        let cfg = Arc::clone(config);
-        let refresh_action = move || set_new_refresh(&cfg, min);
-        let label = format!("{min} minutes");
+    for minutes in [1, 3, 5] {
+        let config = Arc::clone(config);
+        let refresh_action = move || set_new_refresh(&config, minutes);
+        let label = format!("{minutes} minutes");
         tray.add_menu_item(&label, refresh_action).unwrap();
     }
 
@@ -24,10 +24,9 @@ pub fn build_menu(config: &SharedConfig) {
 
 fn set_new_refresh(cfg: &SharedConfig, new_refresh_min: u64) {
     let cfg = Arc::clone(cfg);
-    let refresh_time = new_refresh_min * 60;
     thread::spawn(move || {
         cfg.blocking_lock()
-            .set_refresh_time(Duration::from_secs(refresh_time));
+            .set_refresh_time(Duration::from_secs(new_refresh_min * 60));
         show_notification(new_refresh_min);
     });
 }
